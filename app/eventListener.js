eb16699a73bc474f7aee1d504ad41dc36a31f92a
Vinícius Bohrer <vbsanttos@gmail.com>
2020-03-17 03:14:08 -0300

    eventListener.js refactored

--- app/eventListener.js
+++ app/eventListener.js
@@ -1,42 +1,49 @@
-const getUrl = () => {
-  const url = window.location.href;
-  console.log("getUrl:" + url);
-  return url;
-};
-
-const getUrlSufix = url => {
-  const urlSeparated = url.split("?");
-  const sufix = urlSeparated[1];
-  return sufix ? sufix[sufix.length - 1] : "";
-};
-
-const changeUrl = (url, letter) => {
-  const sufix = "?key=" + letter;
-  const urlSeparated = url.split("?");
-  const nextUrl = urlSeparated[0] + sufix;
-  console.log("URL parts: " + urlSeparated);
-  console.log("changeUrl from: " + url + "\nto: " + nextUrl);
-  window.location.href = nextUrl;
-};
-
-const setPageDetails = (keyName, fromEvent) => {
-  if (keyName !== "") {
-    const letter = document.getElementById("letter1");
-    const letterTitle = document.getElementById("letter2");
-    letter.innerHTML = keyName.toUpperCase();
-    letterTitle.innerHTML = "Diamante feito até a letra:";
-    if (fromEvent) changeUrl(getUrl(), keyName);
-  }
-};
-
+// Adds page event that reloads the page when a key (A-Z) is pressed
 document.addEventListener("keydown", event => {
   const keyName = event.key;
   const keyValue = event.which;
-  console.log("inputValue:", keyValue, "\nkeyName:", keyName);
-  const onlyletters = keyValue >= 65 && keyValue <= 90;
-  if (onlyletters) {
-    setPageDetails(keyName, true);
+  const isLetter = keyValue >= 65 && keyValue <= 90;
+  if (isLetter) {
+    changeUrl(keyName);
   }
 });
 
-setPageDetails(getUrlSufix(getUrl()), false);
+// Change page info
+const setPageInfo = keyName => {
+  const keyPressed = keyName !== "";
+  if (keyPressed) {
+    const letter = document.getElementById("letter1");
+    const letterTitle = document.getElementById("letter2");
+    const letterTip = document.getElementById("letter3");
+    letter.innerHTML = keyName.toUpperCase();
+    letterTitle.innerHTML = "Diamante feito até a letra:";
+    letterTip.innerHTML = "Tente outra letra!";
+    changeUrl(keyName);
+  }
+};
+
+// Get URL and Data passed through URL (HTTP GET METHOD)
+const getUrlSeparated = () => {
+  const url = window.location.href;
+  return url.split("?");
+};
+
+// Get only the data passed through URL
+const getUrlData = () => {
+  const data = getUrlSeparated()[1];
+  return data ? data[data.length - 1] : "";
+};
+
+// Set a new URL based on the key pressed
+const changeUrl = letter => {
+  const currentUrl = getUrlSeparated();
+  const newLetter = "key=" + letter;
+  const sameLetter = currentUrl[1] === newLetter;
+  if (!sameLetter) {
+    const nextUrl = currentUrl[0] + "?" + newLetter;
+    window.location.href = nextUrl;
+  }
+};
+
+// Run this function each time page renders
+setPageInfo(getUrlData());
