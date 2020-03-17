1142088eecd4d8d79af659d2d395542f6003fe33
Vinícius Bohrer <vbsanttos@gmail.com>
2020-03-17 17:07:52 -0300

    Core feature logic refactored

--- app/diamante.php
+++ app/diamante.php
@@ -7,52 +7,29 @@ if (isset($_GET['key'])) {
 
 // Draws diamont recursively
 function drawRecDiamont($diamont, $letter, $currentLetter, $leftSpace, $innerSpace, $topSide) {
-  if ($topSide) {
-    $diamont = $diamont . str_repeat(" ", $leftSpace) . $currentLetter;
-    if($innerSpace>0) {
-      $diamont = $diamont . str_repeat(" ", $innerSpace) . $currentLetter . "\n";
-    } else {
-      $diamont = $diamont . "\n";
-    }
-    if ($letter == $currentLetter) {
-      $currentLetter = chr(ord($currentLetter)-1);
-      $leftSpace = $leftSpace + 1;
-      $innerSpace = $innerSpace - 2;
-      $topSide = false;
-    } else {
-      $currentLetter = chr(ord($currentLetter)+1);
-      $leftSpace = $leftSpace - 1;
-      $innerSpace = $innerSpace + 2;
-      $topSide = true;
-    }
-    return drawRecDiamont($diamont, $letter, $currentLetter, $leftSpace, $innerSpace, $topSide);
+  if($innerSpace > 0) {
+    $diamont .= str_repeat(" ", $leftSpace) . $currentLetter . str_repeat(" ", $innerSpace) . $currentLetter . "\n";
   } else {
-    $diamont = $diamont . str_repeat(" ", $leftSpace) . $currentLetter;
-    if($innerSpace>0) {
-      $diamont = $diamont . str_repeat(" ", $innerSpace) . $currentLetter . "\n";
+    $diamont .= str_repeat(" ", $leftSpace) . $currentLetter . "\n";
+  }
+  if ($topSide) {
+    if ($letter != $currentLetter) {
+      return drawRecDiamont($diamont, $letter, chr(ord($currentLetter)+1), $leftSpace - 1, $innerSpace + 2, true);
     } else {
-      $diamont = $diamont . "\n";
+      return drawRecDiamont($diamont, $letter, chr(ord($currentLetter)-1), $leftSpace + 1, $innerSpace - 2, false);
     }
-    if ($currentLetter == 'A') {
+  } else {
+    if ($currentLetter != 'A') {
+      return drawRecDiamont($diamont, $letter, chr(ord($currentLetter)-1), $leftSpace + 1, $innerSpace - 2, false);
+    } else {
       return $diamont;
-    } else {
-      $currentLetter = chr(ord($currentLetter)-1);
-      $leftSpace = $leftSpace + 1;
-      $innerSpace = $innerSpace - 2;
-      $topSide = false;
     }
-    return drawRecDiamont($diamont, $letter, $currentLetter, $leftSpace, $innerSpace, $topSide);
   }
 }
 
 // function that set variables to optimize recursive function
 function drawDiamont ($letter) {
-  $currentLetter = 'A';
-  $innerSpace = -1;
-  $diamont = "\n";
-  if (strtoupper($letter) == 'A') {
-    echo "A";
-  } else {
-    echo drawRecDiamont($diamont, strtoupper($letter), $currentLetter, ord(strtoupper($letter))-ord($currentLetter), $innerSpace, true);
-  }
+  echo strtoupper($letter) == 'A'
+    ? 'A' 
+    : drawRecDiamont("\n", strtoupper($letter), 'A', ord(strtoupper($letter))-ord('A'), -1, true);
 }
